91ad1052c84170da202bd45cc9468c5cbf08fec7
Vladimir Bubar <vladimir.bubar@kosmoskosmos.de>
2021-04-19 15:23:17 +0200

    [ADD] Hidden templates setting

--- models/Settings.php
+++ models/Settings.php
@@ -1,7 +1,11 @@
 <?php namespace SureSoftware\MailLog\Models;
 
+use Andosto\EventManager\Controllers\EmStats;
+use Cms\Classes\Theme;
 use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use Model;
 
 class Settings extends Model
@@ -12,4 +16,30 @@ class Settings extends Model
 
     public $settingsCode = 'suresoftware_maillog_settings';
     public $settingsFields = 'fields.yaml';
+    protected $jsonable = [
+        'hidden_templates'
+    ];
+
+    public static function getTemplates($relPath) {
+        $theme = Theme::getActiveTheme();
+        $files = File::allFiles($theme->getPath().$relPath);
+        $fileNames = [];
+        foreach ($files as $i => $file) {
+            $content = explode("\n", $file->getContents());
+            $description = "";
+            foreach($content as $line) {
+                if (strpos($line, 'description') !== false) {
+                    $description = trim(explode(' = ', $line)[1]);
+                    $description = substr($description, 1, strlen($description)-2);
+                    break;
+                }
+                if ($line == '==') {
+                    break;
+                }
+            }
+            $fileNames[$file->getPathname()] = $description ? $description : $file->getFilename();
+        }
+        Log::info($fileNames);
+        return $fileNames;
+    }
 }
